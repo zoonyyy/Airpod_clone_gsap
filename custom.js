d15aa09fb5a47b9631aa049e690279e4b1b14e9e
apple <apple@appleui-iMac.skbroadband>
2023-12-20 12:50:05 +0900

    version 0.0.1

---
+++ custom.js
@@ -0,0 +1,9 @@
+const img = document.querySelector("img");
+
+// https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/0001.jpg
+
+// const src = img.getAttribute("src");
+const a = 10;
+const b = a.toString().padStart(4, "0"); //지정된 숫자에 대해 첫번째 파라미터 길이로 두번째 파라미터 문자로 채워줌
+
+console.log(b);
