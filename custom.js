b6d3fc879542e796788afd7fae42b2896e3f2a57
apple <apple@appleui-iMac.local>
2023-12-27 11:17:16 +0900

    231227 version 0.0.2

--- custom.js
+++ custom.js
@@ -1,9 +1,25 @@
-const img = document.querySelector("img");
+// const img = document.querySelector("img");
 
 // https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/0001.jpg
 
-// const src = img.getAttribute("src");
-const a = 10;
-const b = a.toString().padStart(4, "0"); //지정된 숫자에 대해 첫번째 파라미터 길이로 두번째 파라미터 문자로 채워줌
+// const a = 10;
+// const b = a.toString().padStart(4, "0"); // 지정된 숫자에 대해 첫번째 파라미터 길이로 두번째 파라미터 문자로 채워줌
 
-console.log(b);
+// console.log(b);
+
+// Hero섹션높이 지정 : 헤더 높이 지정
+const headerHeight = document.querySelector("header").offsetHeight;
+const heroSection = document.querySelector(".section-01");
+console.log(headerHeight);
+
+heroSection.style.marginTop = headerHeight + 50 + "px";
+
+// GSAP 효과 지정
+const imgObh = document.querySelector("#hero-lightpass");
+const mainText = document.querySelector(".main-elem");
+
+let tl0 = gsap.timeline();
+tl0
+  .add("start0") // 시작 지점 지정
+  .fromTo(imgObj, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0")
+  .fromTo(mainText, { opacity: 0 }, { opacity: 1, duration: 2 }, "start0");
